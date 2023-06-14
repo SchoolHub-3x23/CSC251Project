558b75c4adae3dd7993eebc17b14e58bd7e373e7
SchoolGitHub117 <136639139+SchoolGitHub117@users.noreply.github.com>
2023-06-14 15:53:08 -0400

    Added documentation comments
    Added Javadoc comments and removed old comments.

--- Project 1/TicketPlan.java
+++ Project 1/TicketPlan.java
@@ -1,3 +1,9 @@
+/**
+ * The TicketPlan class implements a ticket plan that can be used to calculate the price of admission for a family and their duration of stay in an amusement park or water park, or both.
+ * It provides methods to retrieve and set information about the family, ticket plan, number of adults and children, and the duration of their stay. 
+ * It also calculates the cost per day for adults and children based on the selected ticket plan and provides a method to calculate the total price of the ticket plan.
+ * @version 1.0
+ */
 public class TicketPlan {
     private String lastName;
     private int ticketPlan; //1 = Amusement Park    2 = Water Park  3 = Both Parks
@@ -5,12 +11,21 @@ public class TicketPlan {
     private int numChildren;
     private int days;
 
-        //No-Arg
-        public TicketPlan() {
+    /**
+     * This is a no-argument constructor that creates an instance of the TicketPlan class without any initial values.
+     */
+    public TicketPlan() {
 
-        }
+    }
 
-    //Contructor that takes an argument for each field
+    /**
+     * This constructs a ticket plan with a specified Family's last name, ticket plan, number of adults, number of children, and how many days they'll stay.
+     * @param lastName (String): The last name of the family.
+     * @param ticketPlan (int): The selected ticket plan (1 for Amusement Park, 2 for Water Park, 3 for Both Parks).
+     * @param numAdults (int): The number of adults attending.
+     * @param numChildren (int): The number of children attending.
+     * @param days (int): The number of days the family will be staying.
+     */
     public TicketPlan(String lastName, int ticketPlan, int numAdults, int numChildren, int days) {
         this.lastName = lastName;
         this.ticketPlan = ticketPlan;
@@ -20,14 +35,27 @@ public class TicketPlan {
     }
 
     //Setters + Getters
+
+    /**
+     * 
+     * @return Returns the last name of the family.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * 
+     * @param lastName Sets the last name of the family.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * 
+     * @return Returns the selected ticket plan as a string (e.g., "Amusement Park Only", "Water Park Only", "Both Parks").
+     */
     public String getTicketPlan() {
         String plan = "";
         if (ticketPlan == 1) {
@@ -42,35 +70,66 @@ public class TicketPlan {
         return plan;
     }
 
+    /**
+     * 
+     * @param ticketPlan Sets the ticket plan (1 for Amusement Park, 2 for Water Park, 3 for Both Parks).
+     */
     public void setTicketPlan(int ticketPlan) {
         this.ticketPlan = ticketPlan;
     }
 
+    /**
+     * 
+     * @return Returns the number of adults attending.
+     */
     public int getNumAdults() {
         return numAdults;
     }
 
+    /**
+     * 
+     * @param numAdults Sets the number of adults attending.
+     */
     public void setNumAdults(int numAdults) {
         this.numAdults = numAdults;
     }
 
+    /**
+     * 
+     * @return Returns the number of children attending.
+     */
     public int getNumChildren() {
         return numChildren;
     }
 
+    /**
+     * 
+     * @param numChildren Sets the number of children attending.
+     */
     public void setNumChildren(int numChildren) {
         this.numChildren = numChildren;
     }
 
+    /**
+     * 
+     * @return Returns the number of days the family will be staying.
+     */
     public int getDays() {
         return days;
     }
 
+    /**
+     * 
+     * @param days Sets the number of days the family will be staying.
+     */
     public void setDays(int days) {
         this.days = days;
     }
 
-    //Returns Cost per day per adult
+    /**
+     * 
+     * @return Returns the cost per day per adult based on the selected ticket plan and the number of days.
+     */
     public double getCostPerDayAdult() {
         double cost = 0;
 
@@ -102,7 +161,10 @@ public class TicketPlan {
         return cost;
     } //End Method
 
-    //Returns Cost per day per child
+    /**
+     * 
+     * @return Returns the cost per day per child based on the selected ticket plan and the number of children.
+     */
     public double getCostPerDayChild() {
         double cost = 0;
 
@@ -133,7 +195,10 @@ public class TicketPlan {
         return cost;
     } //End Method
 
-    //Return total cost
+    /**
+     * 
+     * @return Calculates and returns the total price of the ticket plan based on the cost per day for adults and children and the duration of stay.
+     */
     public double getPlanPrice() {
         return ((getCostPerDayChild() * numChildren) + (getCostPerDayAdult() * numAdults)) * days;
     } //End Method
